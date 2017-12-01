decedb4f734ace4dcf6e1abef621937e11f73be2
Víctor Castaño Gutiérrez <victorcastanogutierrez@gmail.com>
2017-12-01 23:41:21 +0100

    Cambio a test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,31 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { TestPage } from '../pages/test/test';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+  pages: Array<{title: string, component: any}>;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+    this.initApp();
+
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'Test', component: TestPage }
+    ];
+  }
+
+  initApp() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
   }
 }
